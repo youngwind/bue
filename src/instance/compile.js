58593b9f6108f26d831798b7c19f9999f9b9634e
youngwind <540506594@qq.com>
2016-08-30 17:45:50 +0800

    完善compile逻辑

--- src/instance/compile.js
+++ src/instance/compile.js
@@ -2,22 +2,40 @@
  * Created by youngwind on 16/8/18.
  */
 
+let fragment, currentNodeList = [];
+
 exports._compile = function () {
-    this.fragment = document.createDocumentFragment();
+    fragment = document.createDocumentFragment();
+
+    // 用一个栈来存储遍历过程中当前的父节点
+    currentNodeList.push(fragment);
+
     this._compileNode(this.$template);
-    this.$el.innerHTML = '';
-    this.fragment.childNodes.forEach((child) => {
-        this.$el.appendChild(child.cloneNode(true));
-    });
+
+    this.$el.parentNode.replaceChild(fragment, this.$el);
+    this.$el = document.querySelector(this.$options.el);
 };
 
 exports._compileElement = function (node) {
-    this.currentNode = document.createElement(node.tagName);
-    this.fragment.appendChild(this.currentNode);
+    let newNode = document.createElement(node.tagName);
+
+    // 处理节点属性
+    if (node.hasAttributes()) {
+        let attrs = node.attributes;
+        Array.from(attrs).forEach((attr) => {
+            newNode.setAttribute(attr.name, attr.value);
+        });
+    }
+
+
+    let currentNode = currentNodeList[currentNodeList.length - 1].appendChild(newNode);
 
     if (node.hasChildNodes()) {
+        currentNodeList.push(currentNode);
         Array.from(node.childNodes).forEach(this._compileNode, this);
     }
+
+    currentNodeList.pop();
 };
 
 exports._compileText = function (node) {
@@ -35,7 +53,7 @@ exports._compileText = function (node) {
         nodeValue = nodeValue.replace(value, this.$data[property]);
     }, this);
 
-    this.currentNode.appendChild(document.createTextNode(nodeValue));
+    currentNodeList[currentNodeList.length - 1].appendChild(document.createTextNode(nodeValue));
 };
 
 exports._compileNode = function (node) {
--- src/instance/init.js
+++ src/instance/init.js
@@ -6,6 +6,7 @@
 exports._init = function (options) {
     // 其他初始化
 
+    this.$options = options;
     this.$data = options.data;
     this.$el = document.querySelector(options.el);
     this.$template = this.$el.cloneNode(true);
@@ -14,7 +15,7 @@ exports._init = function (options) {
     this.observer = this.observer.create(this.$data);
 
     this.observer.on('set', this.$mount.bind(this));
-    
+
     // 渲染挂载
     this.$mount();
 };
