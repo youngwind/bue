56330aef512ae85477170d56f611d83a116a858b
youngwind <540506594@qq.com>
2016-09-18 14:24:41 +0800

    代码整理

--- src/directives/on.js
+++ src/directives/on.js
@@ -4,10 +4,24 @@
 
 import _ from '../util';
 
+/**
+ * 绑定事件
+ * @param handler
+ */
 exports.update = function (handler) {
     if (typeof handler !== 'function') {
         _.warn(`指令v-on:${this.expression}不是一个函数`);
         return;
     }
-    this.el.addEventListener(this.arg, handler);
+    this.reset();
+    this.handler = handler;
+    this.el.addEventListener(this.arg, this.handler);
+};
+
+/**
+ * 解绑事件
+ */
+exports.reset = function () {
+    if (!this.handler) return;
+    this.el.removeEventListener(this.arg, this.handler);
 };
